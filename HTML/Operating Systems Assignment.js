e886b8e0f325b5d234ebec7793439843a1899cef
DM.Lopez <dmatthew2005@gmail.com>
2021-02-02 20:58:00 -0500

    Added do function.

--- HTML/Operating Systems Assignment.js
+++ HTML/Operating Systems Assignment.js
@@ -1,10 +1,26 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>     
+var inp = parseInt(prompt('Input: '))
+
+while(inp < 1 || inp > 1000) {
+	inp = parseInt(prompt('Invalid input.\nInput: '));
+}
+
+var min = 1;
+var max = 1000;
+count = 0;
+var median;
+
+do {
+	count++;
+	median = Math.floor((min + max)/2);
+	var guess = "Guessed " + median + " - ";
+	if(median < inp) {
+    	guess += "too low."
+    	min = median + 1;
+  } else if(median > inp) {
+    	guess += "too high."
+    	max = median;
+  } else {
+    	guess += "Got it!"
+    	console.log(guess);
+        break;
+  } 
